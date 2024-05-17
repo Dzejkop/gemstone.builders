f139a0ef7a4e244f857259c505af7ea5fe00db60
Dzejkop <jakubtrad@gmail.com>
2024-05-18 00:08:09 +0200

    Start contract work

--- contracts/src/Resource.sol
+++ contracts/src/Resource.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Resource is ERC20 {
-    constructor() ERC20("Name", "SYM") {
+contract Resource is Ownable, ERC20 {
+    constructor(string memory name, string memory symbol) Ownable(msg.sender) ERC20(name, symbol) {
         this;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
