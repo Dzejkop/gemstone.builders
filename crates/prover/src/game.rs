eda1ef8789767b26e769739b269a1b46729d54d1
Dzejkop <jakubtrad@gmail.com>
2024-05-18 10:40:33 +0200

    Initial prover server

---
+++ crates/prover/src/game.rs
@@ -0,0 +1,178 @@
+use serde::{Deserialize, Serialize};
+
+pub const EMPTY: u8 = 0;
+pub const MINE: u8 = 1;
+pub const BELT_DOWN: u8 = 2;
+pub const EXPORTER: u8 = 3;
+
+pub const CARBON: u8 = 0;
+pub const DIAMOND: u8 = 1;
+pub const NUM_RESOURCES: usize = 2;
+
+pub const BOARD_SIZE: usize = 5;
+
+pub type Board = [[u8; BOARD_SIZE]; BOARD_SIZE];
+// Count of resources on each tile, per resource
+pub type State = [[[i32; BOARD_SIZE]; BOARD_SIZE]; NUM_RESOURCES];
+
+pub fn empty_state() -> State {
+    [[[0; BOARD_SIZE]; BOARD_SIZE]; NUM_RESOURCES]
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct GameState {
+    pub board: Board,
+    pub resource_state: State,
+}
+
+impl GameState {
+    pub fn empty() -> Self {
+        Self {
+            board: [[EMPTY; BOARD_SIZE]; BOARD_SIZE],
+            resource_state: empty_state(),
+        }
+    }
+
+    pub fn advance(self) -> Self {
+        let mut new_resource_state = self.resource_state.clone();
+
+        for y in 0..BOARD_SIZE {
+            for x in 0..BOARD_SIZE {
+                match self.board[y][x] {
+                    MINE => {
+                        new_resource_state[CARBON as usize][(y + 1) % BOARD_SIZE][x] += 1;
+                    }
+                    BELT_DOWN => {
+                        for r in 0..NUM_RESOURCES {
+                            new_resource_state[r][(y + 1) % BOARD_SIZE][x] +=
+                                self.resource_state[r][y][x];
+
+                            new_resource_state[r][y][x] -= self.resource_state[r][y][x];
+                        }
+                    }
+                    EXPORTER => {
+                        for r in 0..NUM_RESOURCES {
+                            new_resource_state[r][y][x] -= self.resource_state[r][y][x];
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        Self {
+            board: self.board,
+            resource_state: new_resource_state,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serialize_empty() {
+        const S: &str = indoc::indoc! {r#"
+            {
+                "board": [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ],
+                "resourceState": [
+                    [
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]
+                    ],
+                    [
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]
+                    ]
+                ]
+            }
+        "#};
+        let empty = GameState::empty();
+
+        let empty_s = serde_json::to_string(&empty).unwrap();
+
+        println!("empty_s: {}", empty_s);
+        let deserilized: GameState = serde_json::from_str(S).unwrap();
+
+        assert_eq!(deserilized, empty);
+    }
+
+    #[test]
+    fn advance() {
+        const INIT: &str = indoc::indoc! {r#"
+            {
+                "board": [
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 2, 0],
+                    [0, 0, 0, 2, 0],
+                    [0, 0, 0, 2, 0],
+                    [0, 0, 0, 3, 0]
+                ],
+                "resourceState": [
+                    [
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]
+                    ],
+                    [
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]
+                    ]
+                ]
+            }
+        "#};
+
+        const EXPECTED: &str = indoc::indoc! {
+            r#"
+            [
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0]
+                ],
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            ]
+            "#
+        };
+
+        let initial: GameState = serde_json::from_str(INIT).unwrap();
+
+        let mut state = initial.clone();
+
+        for _ in 0..5 {
+            state = state.advance();
+        }
+
+        let expected: State =
+            serde_json::from_str(EXPECTED).unwrap();
+
+        assert_eq!(state.resource_state, expected);
+    }
+}
--- crates/prover/src/main.rs
+++ crates/prover/src/main.rs
@@ -1,30 +1,119 @@
+use std::net::SocketAddr;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
-use axum::routing::post;
-use axum::Router;
+use alloy_rpc_client::RpcClient;
+use alloy_transport_http::Http;
+use axum::extract::State;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use clap::Parser;
+use game::{Board, GameState};
+use tokio::sync::Mutex;
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
+
+mod game;
 
 struct App {
+    pub is_running: AtomicBool,
+    pub game: Mutex<Option<game::GameState>>,
 }
 
-async fn whatever() {}
+#[tracing::instrument]
+async fn simulate(Json(game): Json<GameState>) -> Json<GameState> {
+    Json(game.advance())
+}
+
+#[tracing::instrument(skip(app))]
+async fn update_board(State(app): State<Arc<App>>, Json(new_board): Json<Board>) {
+    let mut game = app.game.lock().await;
+
+    if let Some(game) = game.as_mut() {
+        game.board = new_board;
+        game.resource_state = game::empty_state();
+    } else {
+        *game = Some(GameState {
+            board: new_board,
+            resource_state: game::empty_state(),
+        });
+    }
+}
+
+#[tracing::instrument(skip(app))]
+async fn delete_board(State(app): State<Arc<App>>) {
+    let mut game = app.game.lock().await;
+    *game = None;
+}
+
+#[tracing::instrument(skip(app))]
+async fn get_board(State(app): State<Arc<App>>) -> Json<Option<Board>> {
+    let game = app.game.lock().await;
+
+    if let Some(game) = game.as_ref() {
+        Json(Some(game.board))
+    } else {
+        Json(None)
+    }
+}
+
+#[tracing::instrument(skip(app))]
+async fn start_game(State(app): State<Arc<App>>) {
+    app.is_running.store(true, Ordering::Relaxed);
+}
+
+#[tracing::instrument(skip(app))]
+async fn stop_game(State(app): State<Arc<App>>) {
+    app.is_running.store(true, Ordering::Relaxed);
+}
+
+#[derive(Debug, Clone, Parser)]
+struct Args {
+    #[clap(short, long, default_value = "0.0.0.0:3123")]
+    pub addr: SocketAddr,
+
+    #[clap(
+        short,
+        long,
+        default_value = "https://ethereum-sepolia.blockpi.network/v1/rpc/public"
+    )]
+    pub rpc_url: String,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
-
     tracing_subscriber::fmt::init();
+
+    let args = Args::parse();
 
     tracing::info!("Prover starting");
 
-    let app = Arc::new(App {});
+
+    let client = Http::new(args.rpc_url.parse()?);
+    let _client: RpcClient<Http<reqwest::Client>> = RpcClient::new(client, false);
+
+    let app = Arc::new(App {
+        is_running: AtomicBool::new(false),
+        game: Mutex::new(None),
+    });
 
     let router = Router::new()
-        .route("/", post(whatever))
-        .with_state(app.clone());
+        .route("/simulate", post(simulate))
+        .route(
+            "/board",
+            get(get_board).post(update_board).delete(delete_board),
+        )
+        .route("/game/start", post(start_game))
+        .route("/game/stop", post(stop_game))
+        .with_state(app.clone())
+        .layer(CorsLayer::permissive())
+        .layer(TraceLayer::new_for_http());
 
-    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(args.addr).await?;
 
-    let listener = tokio::net::TcpListener::bind(addr).await?;
+    tracing::info!(addr = ?args.addr, "Listening");
+
     axum::serve(listener, router)
         .with_graceful_shutdown(common::shutdown_signal())
         .await?;
