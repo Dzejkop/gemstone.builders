0ba24aa95abe3df59716731bf6da82eb91b48f2c
Dzejkop <jakubtrad@gmail.com>
2024-05-18 22:58:39 +0200

    Add image + GH link

--- frontend/src/components/NotConnected.js
+++ frontend/src/components/NotConnected.js
@@ -6,12 +6,18 @@ import {
 function NotConnected() {
     return (
         <header className="App-header">
+            <img src="logo64.png" width={256} className="App-logo" alt="logo" />
             <div className="title">Gamestone Builders</div>
             <div className="subtitle">A resource gathering game with a ZK engine and on-chain global economy</div>
+            <a href="https://github.com/Dzejkop/gemstone.builders">
+                <img src="github-mark-white.png" width={128} className="gh-logo" />
+                <div className="no-decoration white subtitle">Check out the Code!</div>
+            </a>
+
             <div>Connect to play</div>
             <MetaMaskButton theme={"light"} color="white"></MetaMaskButton>
         </header>
-    );  
+    );
 }
 
 export default NotConnected;
